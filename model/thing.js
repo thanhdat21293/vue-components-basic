0e8b1a74fde16cdb6299d244ecbf7af8c6ddf21c
Cuong Trinh <cuong@techmaster.vn>
2017-04-17 15:24:36 +0700

    function getThing

--- model/thing.js
+++ model/thing.js
@@ -9,7 +9,7 @@ class Thing {
 	  this.type = type
   }
 }
-
+//TODO: Tung hãy điền hết các thing và animal vào đây
 let allthings = []
 
 allthings.push(new Thing('Bird', 'bird.png', 2))
@@ -21,4 +21,15 @@ allthings.push(new Thing('Turtle', 'turtle.png', 2))
 allthings.push(new Thing('TV', 'Tv.png', 1))
 
 console.log(allthings)
-//TODO: Tung hãy điền hết các thing và animal vào đây
+//TODO: hãy hoàn một function sau đây. Giao cho đội Vue.js
+/**
+ *
+ * @param type = 0 lấy tất cả
+ * type = 1, trả về thing, không phải animal
+ * type = 2, trả về animal only
+ */
+function getThing(type) {
+
+}
+
+exports.getThing = getThing
