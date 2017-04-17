c0fae6c7eb1a24fd36d6a5b9aa474759277d8cf5
tungptseven <tungpt.seven@gmail.com>
2017-04-17 15:18:45 +0700

    fix add thing and animal

--- model/thing.js
+++ model/thing.js
@@ -18,6 +18,7 @@ allthings.push(new Thing('Car', 'car.png', 1))
 allthings.push(new Thing('Cat', 'cat.png', 2))
 allthings.push(new Thing('Seahorse', 'seahorse.png', 2))
 allthings.push(new Thing('Turtle', 'turtle.png', 2))
-allthings.push(new Thing('TV', 'TV.png', 1))
+allthings.push(new Thing('TV', 'Tv.png', 1))
 
+console.log(allthings)
 //TODO: Tung hãy điền hết các thing và animal vào đây
